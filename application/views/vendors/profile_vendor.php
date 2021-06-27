79e028b02cfe50713508a05b4f3df40407ef0b6e
Ilham Dhiya Ulhaq <ulhaqilhamdhiya@gmail.com>
2021-06-28 00:06:15 +0700

    Menyelesaikan top-menu di class card left

--- application/views/vendors/profile_vendor.php
+++ application/views/vendors/profile_vendor.php
@@ -1,3 +1,4 @@
+
 <div class="cari-vendor">
     <div class="search-option">
         <form action="">
@@ -121,7 +122,7 @@
                                     </div>
                                     <div class="col-10">
                                         <a href="">
-                                            <?php 
+                                            <?php
                                             $website = "http://www.facebook.com/ilhamDhiyaUlhaq";
                                             echo strlen($website) > 28 ? substr($website, 0, 28) . '...' : substr($website, 0, 28);
                                             ?>
@@ -172,11 +173,62 @@
                     </div>
                 </div>
                 <div class="col-md-9">
-                    <div class="card">
-                        <div class="card-body">
-                            This is some text within a card body.
+                    <!-- flex card -->
+                    <?php
+                    $ada = 1;
+                    if ($ada == 1) :
+                    ?>
+                        <div class="card card-left mb-3">
+                            <div class="card-left-1">
+                                <img src="<?= base_url(); ?>assets/vendors/img/news/new_flex_icon.png" class="vendor-flex" alt="">
+                                <p>Vendor ini menyediakan fitur Flexi Reschedule. Hubungi sekarang untuk informasi lebih lanjut!</p>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <!-- / -->
+                    <!-- pembayaran -->
+                    <div class="card mb-3">
+                        <div class="card-left-2">
+                            <div class="img-style-vendor">
+                                <span><i class="fas fa-check-circle"></i> Weddingku</span>
+                            </div>
+                            <div class="pay">
+                                <div class="icon-text">
+                                    <span class="span-1"><i class="fas fa-hourglass-half"></i> Cicilan 0% hingga 24 bulan</span> <small>|</small> <span class="span-2"><i class="far fa-credit-card"></i> Opsi pembayaran lebih banyak</span> <small>|</small> <span class="span-3"><i class="fas fa-file-invoice"></i> Pembayaran aman & mudah</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <!-- / -->
+                    <!--  -->
+                    <div class="card">
+                        <div class="card-left-3">
+                            <div class="top-menu">
+                                <div class="row justify-content-center">
+                                    <div class="col-9">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <a href=""><i class="fas fa-shopping-bag"></i> Produk</a>
+                                            </div>
+                                            <div class="col-3">
+                                                <a href=""><i class="fas fa-tags"></i> Harga</a>
+                                            </div>
+                                            <div class="col-3">
+                                                <a href=""><i class="fas fa-address-card"></i> Tentang Kami</a>
+                                            </div>
+                                            <div class="col-3">
+                                                <a href=""><i class="fas fa-percent"></i> Promo</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                    <!-- / -->
                 </div>
             </div>
         </div>

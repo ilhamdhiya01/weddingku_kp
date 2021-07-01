57f11f7e30178e21c16eaf47a9a85a8e1f2e8dcf
Ilham Dhiya Ulhaq <ulhaqilhamdhiya@gmail.com>
2021-07-01 21:21:37 +0700

    cimmit

---
+++ application/views/produk/index.php
@@ -0,0 +1,21 @@
+<section class="produk-info">
+    <div class="container">
+        <div class="smooth-slider">
+            <div class="row mb-5">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-2">
+                            <a href="">Overview</a>
+                        </div>
+                        <div class="col-2">
+                            <a href="">Detail</a>
+                        </div>
+                        <div class="col-2">
+                            <a href="">Lokasi</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

ae1acb6e5fc2b5315ce64987dfa520a8448b97ad
Ilham Dhiya Ulhaq <ulhaqilhamdhiya@gmail.com>
2021-07-27 00:33:12 +0700

    proses show dan hide password

--- application/views/auth_member/registrasi_member.php
+++ application/views/auth_member/registrasi_member.php
@@ -1,92 +1,145 @@
 <div class="style-bg"></div>
 <div class="registrasi">
-    <div class="row justify-content-center">
-        <div class="col-9">
-            <div class="row judul">
-                <div class="col-md-7">
-                    <h4>Persiapkan Pernikahan dengan Beragam Kemudahan & Penawaran Eksklusif</h4>
-                    <p>Daftar sekarang & dapatkan harga terbaik untuk<br>lengkapi kebutuhan pernikahan Anda</p>
-                    <div class="icon-info">
-                        <div class="row">
-                            <div class="col-md-6 info">
-                                <img src="<?= base_url(); ?>assets/vendors/img/news/register1.png" class="icon" alt="">
-                                <div class="h5-p">
-                                    <h5>Vendor & Produk Pernikahan Terlengkap</h5>
-                                    <p>Lengkapi kebutuhan pernikahan Anda dengan <b>5.000+ pilihan produk dari 20.000</b> vendor pernikahan profesional</p>
-                                </div>
-                            </div>
-                            <div class="col-md-6 info">
-                                <img src="<?= base_url(); ?>assets/vendors/img/news/register2.png" class="icon" alt="">
-                                <div class="h5-p">
-                                    <h5>Sesuaikan Pesanan dengan Impian Anda</h5>
-                                    <p>Ingin menyesuaikan pesanan dengan kebutuhan? <b>Konsultasikan</b> bersama vendor & <b>pesan produk</b> sesuai keinginan Anda</p>
-                                </div>
-                            </div>
-                            <div class="col-md-6 info">
-                                <img src="<?= base_url(); ?>assets/vendors/img/news/register3.png" class="icon" alt="">
-                                <div class="h5-p">
-                                    <h5>Promo Eksklusif & Hadiah Menarik</h5>
-                                    <p>Jangan lewatkan kesempatan untuk memesan vendor dengan promo eksklusif di Bridestory & beragam hadiah menarik <b>selama online event!</b></p>
-                                </div>
-                            </div>
-                            <div class="col-md-6 info">
-                                <img src="<?= base_url(); ?>assets/vendors/img/news/register4.png" class="icon" alt="">
-                                <div class="h5-p">
-                                    <h5>Cicilan 0% hingga 24 bulan</h5>
-                                    <p>Nikmati opsi pembayaran dengan cicilan 0% hingga 24 bulan serta <b>lebih dari 30 metode</b> lainnya menggunakan Bridestory Pay</p>
-                                </div>
-                            </div>
+    <div class="row judul justify-content-center">
+        <div class="col-md-7">
+            <h4>Persiapkan Pernikahan dengan Beragam Kemudahan & Penawaran Eksklusif</h4>
+            <p>Daftar sekarang & dapatkan harga terbaik untuk<br>lengkapi kebutuhan pernikahan Anda</p>
+            <div class="icon-info">
+                <div class="row">
+                    <div class="col-md-6 info">
+                        <img src="<?= base_url(); ?>assets/vendors/img/news/register1.png" class="icon" alt="">
+                        <div class="h5-p">
+                            <h5>Vendor & Produk Pernikahan Terlengkap</h5>
+                            <p>Lengkapi kebutuhan pernikahan Anda dengan <b>5.000+ pilihan produk dari 20.000</b> vendor pernikahan profesional</p>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-5">
-                    <div class="card form-register">
-                        <div class="card-body">
-                            <div class="form-header">
-                                <div class="row">
-                                    <div class="col-6">
-                                        <h5>Daftar</h5>
-                                    </div>
-                                    <div class="col-6">
-                                        <a href="">Masuk</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-input">
-                                <form action="">
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan">
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Alamat Email">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Kata Sandi">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Konfirmasi Kata Sandi">
-                                    </div>
-                                    <button type="submit" class="btn btn-block">Lanjutkan</button>
-                                    <p class="p1">Dengan mendaftar, saya menyetujui Syarat<br> dan Ketentuan Bridestory</p>
-                                    <p class="p2">Sudah punya akun ? <a href="">Masuk</a></p>
-                                </form>
-                            </div>
+                    <div class="col-md-6 info">
+                        <img src="<?= base_url(); ?>assets/vendors/img/news/register2.png" class="icon" alt="">
+                        <div class="h5-p">
+                            <h5>Sesuaikan Pesanan dengan Impian Anda</h5>
+                            <p>Ingin menyesuaikan pesanan dengan kebutuhan? <b>Konsultasikan</b> bersama vendor & <b>pesan produk</b> sesuai keinginan Anda</p>
                         </div>
-                        <div class="card-footer">
-                            <span>Punya bisnis terkait pernikahan ? <a href="">Bergabung sebagai vendor</a></span>
+                    </div>
+                    <div class="col-md-6 info">
+                        <img src="<?= base_url(); ?>assets/vendors/img/news/register3.png" class="icon" alt="">
+                        <div class="h5-p">
+                            <h5>Promo Eksklusif & Hadiah Menarik</h5>
+                            <p>Jangan lewatkan kesempatan untuk memesan vendor dengan promo eksklusif di Bridestory & beragam hadiah menarik <b>selama online event!</b></p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 info">
+                        <img src="<?= base_url(); ?>assets/vendors/img/news/register4.png" class="icon" alt="">
+                        <div class="h5-p">
+                            <h5>Cicilan 0% hingga 24 bulan</h5>
+                            <p>Nikmati opsi pembayaran dengan cicilan 0% hingga 24 bulan serta <b>lebih dari 30 metode</b> lainnya menggunakan Bridestory Pay</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-5">
+            <div class="card form-register">
+                <div class="card-body">
+                    <div class="form-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>Daftar</h5>
+                            </div>
+                            <div class="col-6">
+                                <a href="">Masuk</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-input">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Alamat Email">
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-11">
+                                        <input type="password" class="form-control" id="pass" aria-describedby="emailHelp" placeholder="Kata Sandi">
+                                    </div>
+                                    <div class="col-1">
+                                        <span id="show-password" onclick="show()">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-11">
+                                        <input type="password" class="form-control" id="konfirmasi-pass" aria-describedby="emailHelp" placeholder="Konfirmasi Kata Sandi">
+                                    </div>
+                                    <div class="col-1">
+                                        <span id="show-password-2">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-block">Lanjutkan</button>
+                            <p class="p1">Dengan mendaftar, saya menyetujui Syarat<br> dan Ketentuan Bridestory</p>
+                            <p class="p2">Sudah punya akun ? <a href="">Masuk</a></p>
+                        </form>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <span>Punya bisnis terkait pernikahan ? <a href="">Bergabung sebagai vendor</a></span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+<div class="chat-fixed">
+    <a href="" class="chat"><i class="fab fa-whatsapp"></i> chat</a>
+</div>
+<!-- show password -->
+<script>
+    function show() {
+        let x = document.getElementById('pass').type;
+        if (x == 'password') {
+            document.getElementById('pass').type = 'text';
+            document.getElementById('show-password').innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-slash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z"/>
+                                                                    <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z"/>
+                                                                    </svg>`;
+        } else {
+            document.getElementById('pass').type = 'password';
+            document.getElementById('show-password').innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                                    <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                                                </svg>`;
+        }
+    }
+    $('#show-password-2').click(function(){
+        let x = $('#konfirmasi-pass').attr('type');
+        let icon2 = ``;
+        if(x == 'password') {
+            $('#konfirmasi-pass').attr('type','text');
+            $('#show-password-2').text('');
+        } else {
+            $('#konfirmasi-pass').attr('type','password');
+        }
+    });
+</script>

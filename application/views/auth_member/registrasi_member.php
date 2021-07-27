95aa1cf9423cceec3f9c173f2159b78552865b1b
Ilham Dhiya Ulhaq <ulhaqilhamdhiya@gmail.com>
2021-07-27 21:05:56 +0700

    icon

--- application/views/auth_member/registrasi_member.php
+++ application/views/auth_member/registrasi_member.php
@@ -74,10 +74,7 @@
                                     </div>
                                     <div class="col-1">
                                         <span id="show-password" onclick="show()">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                                                <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                                            </svg>
+                                            <i class="fas fa-eye"></i>
                                         </span>
                                     </div>
                                 </div>
@@ -88,12 +85,9 @@
                                         <input type="password" class="form-control" id="konfirmasi-pass" aria-describedby="emailHelp" placeholder="Konfirmasi Kata Sandi">
                                     </div>
                                     <div class="col-1">
-                                        <span id="show-password-2">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                                                <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                                            </svg>
-                                        </span>
+
+                                        <i class="fas fa-eye fa-eye-2" id="show-password-2"></i>
+
                                     </div>
                                 </div>
                             </div>
@@ -119,27 +113,21 @@
         let x = document.getElementById('pass').type;
         if (x == 'password') {
             document.getElementById('pass').type = 'text';
-            document.getElementById('show-password').innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-slash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                                    <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
-                                                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z"/>
-                                                                    <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z"/>
-                                                                    </svg>`;
+            document.getElementById('show-password').innerHTML = `<i class="fas fa-eye-slash"></i>`;
         } else {
             document.getElementById('pass').type = 'password';
-            document.getElementById('show-password').innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                                                                    <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                                                                </svg>`;
+            document.getElementById('show-password').innerHTML = `<i class="fas fa-eye"></i>`;
         }
     }
-    $('#show-password-2').click(function(){
+
+    $('#show-password-2').click(function() {
         let x = $('#konfirmasi-pass').attr('type');
-        let icon2 = ``;
-        if(x == 'password') {
-            $('#konfirmasi-pass').attr('type','text');
-            $('#show-password-2').text('');
+        let icon2 = '<i class="fas fa-eye-slash"></i>';
+        if (x == 'password') {
+            $('#konfirmasi-pass').attr('type', 'text');
+            $('#show-password-2 i').addClass('fas fa-eye-slash');
         } else {
-            $('#konfirmasi-pass').attr('type','password');
+            $('#konfirmasi-pass').attr('type', 'password');
         }
     });
 </script>
